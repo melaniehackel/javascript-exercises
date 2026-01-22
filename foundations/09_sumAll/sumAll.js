79a384b2411ea7b5d1384fe106cf9236103aae9d
Meli <m.hackel999@gmail.com>
2026-01-22 13:33:27 +0100

    finish ninth exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    if (firstNum < 0 || secondNum < 0) return "ERROR";
+    let smallerNum = Math.min(firstNum, secondNum);
+    let biggerNum = Math.max(firstNum, secondNum);
+    let total = 0;
+    for (let i = smallerNum; i <= biggerNum; i++){
+        total += i;
+    }
+    return total;
 
 };
 
