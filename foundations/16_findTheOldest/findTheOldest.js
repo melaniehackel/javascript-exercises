70b22ea51579f29b1aa5e8ca57995f9b37bb8870
Meli <m.hackel999@gmail.com>
2026-01-29 12:08:54 +0100

    finish sixteenth exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest_year = Math.max(...array.map(function(person) 
+        {if (person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person.yearOfDeath - person.yearOfBirth}))
+    
+    return array.find((person) => person.yearOfDeath - person.yearOfBirth === oldest_year);
+    
 
 };
 
