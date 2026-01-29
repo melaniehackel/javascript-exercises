81f86129c3d79f7757c10c5b368fed981f3e9090
Meli <m.hackel999@gmail.com>
2026-01-29 11:38:21 +0100

    finish fourteenth exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0){
+        return 0;
+    }
+    else if (num < 0){
+        return "OOPS";
+    }
+    let i = 1;
+    let j = 1;
+    for (let c = 0; c < num - 1; c++){
+        let oldJ = j;
+        j = i + j;
+        i = oldJ;
+    }
+    return i;
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
